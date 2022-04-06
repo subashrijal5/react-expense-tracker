a34ccbd149a90f83a12702ceb9db543c96562faf
Subash Rijal <subashrijal5@gmail.com>
2022-04-06 23:46:54 +0545

    some issues fixes

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ChartBar from './ChartBar';
 import './Chart.css';
 const Chart = (props) => {
--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -3,12 +3,12 @@ import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '.././Card';
 function ExpenseItem(props) {
-    const [title, setTitle] = useState(props.expense.title);
+    // const [title, setTitle] = useState(props.expense.title);
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.expense.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.expense.title}</h2>
                 <div className="expense-item__price">${props.expense.amount}</div>
             </div>
         </Card>
